5f16457cf5ba7a094a18dd7631630c049ef01fa6
Natanael Neto <natanael@lti.net.br>
2020-10-27 12:19:37 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursosService } from './cursos/cursos.service';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { InputPropertyComponent } from './input-property/input-property.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursosComponent,
+    DataBindingComponent,
+    InputPropertyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+  url: any = 'http://www.google.com';
+  valorAtual: string;
+  valorSalvo: string;
+  urlImagem = 'http://lorempixel.com/400/200/sports';
+  isMouseOver: boolean = false;
+  nome: string = 'nome qualquer';
+
+  nomeCurso: string = 'Angular';
+
+  getValor() {
+    return 1;
+  }
+
+  DoILikeAngular() {
+    return true;
+  }
+
+  salvarValor(valor) {
+    this.valorSalvo = valor;
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  botaoClicado() {
+    window.alert('Botão');
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver
+  }
+
+  pessoa = {
+    nome: "Natan",
+    idade: 20
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
